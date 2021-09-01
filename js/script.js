a798e5d282c295709b355652c809f99e2b23fbcd
razon1494 <razon1494@gmail.com>
2021-09-02 05:49:26 +0600

    Function Added

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
-const toggleSpinner = displayStyle => {
-    document.getElementById('spinner').style.display = displayStyle;
-}
-
+//function for getElementById
 const getElement = id => {
-    return document.getElementById(`${id}`);
-}
-
+        return document.getElementById(`${id}`);
+    }
+    //function for loading animation
+const toggleSpinner = displayStyle => {
+        getElement('spinner').style.display = displayStyle;
+    }
+    //button clickhandler for searching books
 const searchBook = () => {
-    const inputField = document.getElementById('search-field');
+    const inputField = getElement('search-field');
     const inputText = inputField.value;
     //clearing screen
     inputField.value = '';
-    //getElement is a function
     getElement('number-of-results').innerHTML = '';
     getElement('search-result').innerHTML = '';
+    //spinner show
     toggleSpinner('block');
-    //empty input handle
+    //empty input error handle
     if (inputText === '') {
-
+        getElement('number-of-results').innerHTML = `<h3>You Have Not Write Anything. Search again.</h3>`;
+        toggleSpinner('none');
     } else {
+        //API call and pass to find data
         const url = `https://openlibrary.org/search.json?q=${inputText}`;
         fetch(url)
             .then(res => res.json())
@@ -35,6 +38,7 @@ const findResult = Allresults => {
         numberOfResult.innerHTML = `<h3>No Result Found</h3>`
         toggleSpinner('none');
     } else {
+        //number of book line display and pass for showing results
         numberOfResult.innerHTML = `<h3 class="fw-bold mb-3 py-3">Total ${numberOfResultsFound} Books Found</h3>`;
         showResult(Allresults.docs);
     }
@@ -43,19 +47,18 @@ const findResult = Allresults => {
 //trying to show results
 const showResult = books => {
     const parentDiv = getElement('search-result');
-    // parentDiv.textContent = '';
     //working on all books (docs array) using foreach loop to show each book
     books.forEach(book => {
         //first check for cover
         let coverUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+        //if image not found  error handle
         if (book.cover_i === undefined) {
-            coverUrl = `notFound.png`;
+            coverUrl = `images/notFound.png`;
         }
-        //book name
-        const bookName = book.title;
 
-        //author name, publisher name, publisher years using destructuring (it can be found or not)
-        let { author_name, publisher, first_publish_year } = book;
+        /*  book title, author name, publisher name, publisher years using destructuring */
+        let { title, author_name, publisher, first_publish_year } = book;
+
         // Check author is found or not
         if (author_name === undefined) {
             author_name = 'Author Not Found'
@@ -73,7 +76,7 @@ const showResult = books => {
         if (first_publish_year === undefined) {
             first_publish_year = 'Not Found'
         }
-        //create child div of result to add
+        //create child div of result to show
         const child = document.createElement('div');
         //class adding on the child div.(showing result)
         child.classList.add('card');
@@ -82,7 +85,7 @@ const showResult = books => {
         child.innerHTML = `
         <img src="${coverUrl}" class="card-img-top w-75 h-75 mt-2 rounded mx-auto" alt="Cover Not Available" >
         <div class="card-body">
-                <h5 class="card-title">${bookName}</h5>
+                <h5 class="card-title">${title}</h5>
                 <p class="card-text">Author Name: ${author_name}</p>
                 <p class="card-text">Publisher: ${publisher}</p>
                 <p class="card-text">First Publish: ${first_publish_year}</p>
@@ -91,6 +94,6 @@ const showResult = books => {
         `
         parentDiv.appendChild(child);
     });
+    //spinner off
     toggleSpinner('none');
-
 }
